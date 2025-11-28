20c6e5ef4af441a1c28c61c24f4605e29e5d0bbe
Ariel <arielamriou@gmail.com>
2025-11-28 21:48:40 +0100

    feat: change of logo in title

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import { defineConfig } from 'astro/config';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  vite: {
-    plugins: [svgr()],
-  },
+    vite: {
+        plugins: [svgr()],
+    },
+    server: {
+        allowedHosts: [
+            "pseudophilosophical-overstimulatively-azzie.ngrok-free.dev"
+        ]
+    }
 });
